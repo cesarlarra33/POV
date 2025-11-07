8096e19df7e00041bc704e5b6f25ead73f1759df
bouchraezz <bouchra.ezzagmouny@bordeaux-inp.fr>
2025-11-07 17:32:55 +0100

    USART done

--- mbi5024.h
+++
@@ -1,8 +0,0 @@
-#ifndef MBI5024_H
-#define MBI5024_H
-#include <stdint.h>
-#include <stdbool.h>
-void mbi5024_init(void);                // configure OE, LE + SPI
-void mbi5024_display_enable(bool on);   // on=true -> OE=0 (affiche)
-void mbi5024_write16(uint16_t bits);    // shift 16 bits + latch
-#endif
--- spi_avr.h
+++
@@ -1,6 +0,0 @@
-#ifndef SPI_AVR_H
-#define SPI_AVR_H
-#include <stdint.h>
-void spi_init(uint8_t mode, uint8_t clk_div); // mode 0..3 ; clk_div: 2,4,8,16,32,64,128
-void spi_send_byte(uint8_t v);
-#endif
