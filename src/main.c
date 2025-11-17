5d71759426034cfe34d57465d2b9b531c78cbca2
LARRAGUETA Cesar <Cesar.Larragueta@bordeaux-inp.fr>
2025-11-17 13:50:24 +0100

    ajout d'un main qui affiche le template du cadran d'horloge sans les aiguilles

--- src/main.c
+++ src/main.c
@@ -1,19 +1,15 @@
 #include "utils.h"
 #include "hall_sensor.h"
 #include "MBI5024.h"
+#include "clock_template.h"
 
 
 int main(void) {
     set_all_leds_outputs();
     set_HALL_as_input();
 
-    uint16_t full_on_mask = 0;
-    for (uint8_t i = 0; i < NB_LEDS; ++i) {
-        full_on_mask |= (uint16_t)(1U << i);
-    }
-
     while (1) {
-        display_mask_at_angle(full_on_mask, 90U); // refresh the image at 90 degrees
+        display_patterns(clock_base_pattern, (uint16_t)CLOCK_PATTERN_SIZE);
     }
 
     return 0;
