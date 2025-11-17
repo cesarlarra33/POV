197992f046fa96cca2b945bae9366f6fbca3ab63
LARRAGUETA Cesar <Cesar.Larragueta@bordeaux-inp.fr>
2025-11-17 13:36:15 +0100

    stockage en PROGMEM des patterns

--- src/pattern.c
+++ src/pattern.c
@@ -1,8 +1,10 @@
+#include <avr/pgmspace.h>
+
 #include "pattern.h"
 
 #define FULL_ON_MASK 0xFFFFU
 #define FULL_ON_ANGLE_DEG 90U
 
-const pattern_t pattern_dict[PATTERN_DICT_SIZE] = {
+const pattern_t pattern_dict[PATTERN_DICT_SIZE] PROGMEM = {
     { FULL_ON_ANGLE_DEG, FULL_ON_MASK }
 };
--- src/pattern.h
+++ src/pattern.h
@@ -2,15 +2,17 @@
 #define PATTERN_H
 
 #include <stdint.h>
+#include <avr/pgmspace.h>
+
 #include "utils.h"
 
 typedef struct {
-    uint16_t angle; // angle logique en degrés
+    int16_t angle;  // angle logique en degrés (peut inclure de petits offsets négatifs)
     uint16_t mask;  // bit i -> LED i (1 = allumé)
 } pattern_t;
 
 #define PATTERN_DICT_SIZE 1
 
-extern const pattern_t pattern_dict[PATTERN_DICT_SIZE];
+extern const pattern_t pattern_dict[PATTERN_DICT_SIZE] PROGMEM;
 
 #endif
