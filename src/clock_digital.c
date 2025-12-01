8c14472ca1599045b2c84ea6cfa3df9ca4eba842
bouchraezz <bouchra.ezzagmouny@bordeaux-inp.fr>
2025-12-01 20:01:05 +0100

    affichage numérique

--- src/clock_digital.c
+++ src/clock_digital.c
@@ -10,18 +10,16 @@
 
 #define QUART_SIZE (THETA_RESOLUTION / 4)
 
-// pattern_t de l'affichage digital courant dans lequel on viendra coller
-// les digits en fonction de leur position.
 pattern_t digital_clock_pattern[THETA_RESOLUTION] = {0};
 
 
 void update_digital_clock(void) {
-    static uint8_t last_minute = 255;// valeur impossible comme ca on force le 1er update
+    static uint8_t last_minute = 255;
 
     uint8_t hour   = current_time.hours;
     uint8_t minute = current_time.minutes;
 
-    // on recalcuule que si les min ont changé car on affiche pas les secondes icit
+    
     if (minute == last_minute) {
         return;
     }
@@ -32,7 +30,7 @@ void update_digital_clock(void) {
     uint8_t m_tens  = minute / 10;
     uint8_t m_units = minute % 10;
 
-    // tableaux de pointeurs pour chaque quart du tour
+   
     const pattern_t* digit_quart_HG[10] = {
         number_0_Q1, number_1_Q1, number_2_Q1, number_3_Q1, number_4_Q1,
         number_5_Q1, number_6_Q1, number_7_Q1, number_8_Q1, number_9_Q1
@@ -52,21 +50,21 @@ void update_digital_clock(void) {
 
     
 
-    // on va copier les bon tableaux de patterns dans digital_clock_pattern, aux bon indices correspondant aux quarts. 
+    
     memcpy_P(&digital_clock_pattern[0 * QUART_SIZE],
              digit_quart_HG[h_tens],
-             QUART_SIZE * sizeof(pattern_t)); // Nord→Ouest
+             QUART_SIZE * sizeof(pattern_t)); 
 
     memcpy_P(&digital_clock_pattern[1 * QUART_SIZE],
              digit_quart_BG[m_tens],
-             QUART_SIZE * sizeof(pattern_t)); // Ouest→Sud
+             QUART_SIZE * sizeof(pattern_t)); 
 
     memcpy_P(&digital_clock_pattern[2 * QUART_SIZE],
              digit_quart_BD[m_units],
-             QUART_SIZE * sizeof(pattern_t)); // Sud→Est
+             QUART_SIZE * sizeof(pattern_t)); 
 
     memcpy_P(&digital_clock_pattern[3 * QUART_SIZE],
              digit_quart_HD[h_units],
-             QUART_SIZE * sizeof(pattern_t)); // Est→Nord
+             QUART_SIZE * sizeof(pattern_t));
 
 }
--- src/clock_digital.h
+++ src/clock_digital.h
@@ -2,28 +2,23 @@
 #define CLOCK_DIGITAL_H
 
 #include "pattern.h"
-
-#define THETA_RESOLUTION 120// nombre de pattenr_t pour faire un tour complet (360°)
+#define THETA_RESOLUTION 120
 
 #define CARTESIAN_DIGIT_HEIGHT 16
 #define CARTESIAN_DIGIT_WIDTH 16
 
-// struct qui sert à décrire une position cartésienne
+
 typedef struct {
     int8_t x;
     int8_t y;
 } cartesian_point_t;
 
-// positionsn des digits réprésentants les unités/dizaines des heures/minutes
+
 extern const cartesian_point_t hour_tens_pos;
 extern const cartesian_point_t hour_units_pos;
 extern const cartesian_point_t min_hour_pos;
 extern const cartesian_point_t min_units_pos;
 
-
-
-// pattern_t de l'affichage digital courant dans lequel on viendra coller la conversion cartésienne / polaire 
-// des digits dessinés en cartésien 
 pattern_t digital_clock_pattern[THETA_RESOLUTION];
 
 /// @brief fonction de maj de l'affichage digital
