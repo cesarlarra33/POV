7b6a0c593eafaf9b45d4c7ba5420ea483b51ae17
LARRAGUETA Cesar <Cesar.Larragueta@bordeaux-inp.fr>
2025-12-01 11:57:45 +0100

    ajout des chiffres en polaires générés par python

---
+++ src/digits.h
@@ -0,0 +1,53 @@
+// digits.h
+#pragma once
+#ifndef DIGITS_H
+#define DIGITS_H
+
+#include "pattern.h"
+
+extern const pattern_t number_0_Q1[];
+extern const pattern_t number_0_Q2[];
+extern const pattern_t number_0_Q3[];
+extern const pattern_t number_0_Q4[];
+
+extern const pattern_t number_1_Q1[];
+extern const pattern_t number_1_Q2[];
+extern const pattern_t number_1_Q3[];
+extern const pattern_t number_1_Q4[];
+
+// ... idem pour 2 à 9 ...
+extern const pattern_t number_2_Q1[];
+extern const pattern_t number_2_Q2[];
+extern const pattern_t number_2_Q3[];   
+extern const pattern_t number_2_Q4[];
+extern const pattern_t number_3_Q1[];
+extern const pattern_t number_3_Q2[];
+extern const pattern_t number_3_Q3[];   
+extern const pattern_t number_3_Q4[];
+extern const pattern_t number_4_Q1[];       
+extern const pattern_t number_4_Q2[];
+extern const pattern_t number_4_Q3[];
+extern const pattern_t number_4_Q4[];   
+extern const pattern_t number_5_Q1[];       
+extern const pattern_t number_5_Q2[];
+extern const pattern_t number_5_Q3[];   
+extern const pattern_t number_5_Q4[];
+extern const pattern_t number_6_Q1[];       
+extern const pattern_t number_6_Q2[];
+extern const pattern_t number_6_Q3[];   
+extern const pattern_t number_6_Q4[];
+extern const pattern_t number_7_Q1[];       
+extern const pattern_t number_7_Q2[];
+extern const pattern_t number_7_Q3[];   
+extern const pattern_t number_7_Q4[];
+extern const pattern_t number_8_Q1[];       
+extern const pattern_t number_8_Q2[];
+extern const pattern_t number_8_Q3[];   
+extern const pattern_t number_8_Q4[];
+extern const pattern_t number_9_Q1[];       
+extern const pattern_t number_9_Q2[];
+extern const pattern_t number_9_Q3[];   
+extern const pattern_t number_9_Q4[];   
+
+#endif // DIGITS_H
+
